e00ba1489f0eeb5800640ddd86d020628da2aa9d
Mary420042 <maryyjane1533@gmail.com>
2025-01-23 17:37:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const formSorteado = document.getElementById('formSorteado');
+    const listaContemplados = document.getElementById('listaContemplados').getElementsByTagName('tbody')[0];
+
+    formSorteado.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const nome = document.getElementById('nome').value;
+        const celular = document.getElementById('celular').value;
+        adicionarSorteado(nome, celular);
+    });
+
+    function adicionarSorteado(nome, celular) {
+        const row = listaContemplados.insertRow();
+        const cellNome = row.insertCell(0);
+        const cellCelular = row.insertCell(1);
+
+        cellNome.textContent = nome;
+        cellCelular.textContent = celular.slice(0, -2) + '**';
+
+        row.addEventListener('click', function() {
+            cellCelular.textContent = celular;
+        });
+    }
+});
